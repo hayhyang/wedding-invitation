5a71d3368f4b4a0116324076521ae645fad2e319
hay <hayhyang@gmail.com>
2023-01-28 19:13:59 +0900

    style: global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from "./style/GlobalStyle";
+
 function App() {
-  return <div className="App">asdfad</div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      asdfad
+    </div>
+  );
 }
 
 export default App;
--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
-
+    ${reset}
 `;
