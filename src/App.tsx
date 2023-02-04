fbdafcfb2fabc8b5c805be9e8677c99b1cb53cd8
hay <hayhyang@gmail.com>
2023-02-04 12:31:27 +0900

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import styled from "styled-components";
+
 import { GlobalStyle } from "./style/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      asdfad
+      <Home>
+        <Video></Video>
+        <Contents></Contents>
+      </Home>
     </div>
   );
 }
+
+const Home = styled.div``;
+const Video = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: gray;
+`;
+const Contents = styled.div`
+  min-height: 100vh;
+`;
 
 export default App;
